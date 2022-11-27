8eb947ebd358965cf3e90bd895a942902a14ab60
Raizal I.N. Pregnanta <raizal.pregnanta@gmail.com>
2022-11-27 15:29:19 +0700

    feat(header): adjust color, size and spacing

--- src/common/components/composition/Header.tsx
+++ src/common/components/composition/Header.tsx
@@ -4,15 +4,15 @@ import {styled} from 'nativewind';
 import Stethoscope from '@icons/Stethoscope.svg';
 import ArrowBack from '@icons/ArrowBack.svg';
 
-const IconStethoscope = styled(Stethoscope, 'fill-brand-primary-400');
-const IconArrowBack = styled(ArrowBack, 'color-brand-primary-400');
+const IconStethoscope = styled(Stethoscope, 'fill-brand-primary-500');
+const IconArrowBack = styled(ArrowBack, 'color-brand-primary-500');
 
 const Header = () => {
   return (
-    <Flex tw="flex flex-row items-center pt-4 pb-2 px-3 bg-white">
+    <Flex tw="flex flex-row items-center pt-4 pb-3 px-3 bg-white">
       <IconArrowBack width={32} height={32} />
-      <H2 tw="mx-4 flex-1 text-brand-primary-900">Book a Doctor</H2>
-      <IconStethoscope width={42} height={42} />
+      <H2 tw="mx-4 flex-1 text-xl text-gray-600 leading-5">Book a Doctor</H2>
+      <IconStethoscope width={48} height={48} />
     </Flex>
   );
 };
